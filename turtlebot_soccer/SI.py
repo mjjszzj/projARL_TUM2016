706bf2cd44b582d1bc95e78a4dfd04a5e1a20aa7
Mingpan <mingpanguo@gmail.com>
2016-06-10 14:52:39 +0200

    SARSA-lambda UNdegbuged

--- turtlebot_soccer/SI.py
+++ turtlebot_soccer/SI.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python
-'''
-Simulation Interface
-
-Description:
-ROS node, handling real-time information about the ball
-
-Publishing:
-ball_pose: pose of the ball, [X, Y, theta]: position + orientation
-ball_vel: linear instantaneous velocity of the ball, [X, Y]
-'''
 
 import rospy
 import array
@@ -22,10 +12,70 @@ from sensor_msgs.msg import JointState
 from std_msgs.msg import String
 import rosbag
 import math
+import random
 
-global PI
-PI = math.pi
+'''
+TurtleBot Agent
 
+Description:
+Class, called by SI, defining agent's behavior & knowledge, SARSA-lambda
+'''
+class TB(object):
+    
+    '''turtlebot's knowledge'''
+    epsilon = 0.3 # how greedy tb is
+    alpha = 0.01 # learning rate
+    gamma = 0.9 # discount factor
+    l = 0.7 # lambda for SARSA(l)
+    
+    state_dim = 3. # 3-dim continuous state for task 1
+    action_num = 2.  # discrete actions of 0/1
+    state = np.zeros(self.state_dim)
+    eli = np.zeros(self.state_dim) # eligibility trace
+    action = 0.
+    
+    '''func approximator'''
+    q_w = np.zeros((self.action_num, self.state_dim)) # 2nd order linear func parameter
+    def qFunc(self, s, derivative = False):
+        s_ex = np.concatenate((np.power(s,2),s,[1.])) # the feature vector
+        if derivative:
+            return s_ex
+        return np.sum(self.q_w*s_ex, axis=1)
+
+    '''take actions'''
+    def actGreedy(self):
+        return np.argmax(self.qFunc(self.state)) # take action w.r.t max Q-value
+
+    def actRandom(self):
+        return int(random.random()*self.action_num) # take random action
+        
+    '''run 1 step w.r.t. given observations'''
+    def runSARSA(self, s, r=0, start=False, done=False):
+        if start:
+            self.state = s; self.action = self.actRandom; return self.action
+        if random.random()>self.epsilon: # epsilon greedy
+            a = self.actGreedy()
+        else:
+            a = self.actRandom()
+        dt = self.gamma*self.qFunc(s)[a] + r - self.qFunc(self.state)[self.action]
+        self.eli = self.gamma * self.l * self.eli + self.qFunc(self.state, derivative=True) # update eligibility trace
+        self.q_w += self.alpha * dt * self.eli # update func parameter
+        self.state = s; self.action = a; return a
+    
+    def __init__(self):
+        # nothing needed
+
+
+'''
+Simulation Interface
+
+Description:
+ROS node, handling real-time information about the ball
+
+Publishing:
+ball_pose: pose of the ball, [X, Y, theta]: position + orientation
+ball_vel: linear instantaneous velocity of the ball, [X, Y]
+'''
 class SI(object):
 	''' Data for the Turtlebot'''
 	'''
@@ -39,7 +89,7 @@ class SI(object):
 	tb_ang = Quaternion()
 	
 	# left and right wheel
-	angLeft = 0.0 # positive firection: clockwise: Left - Right
+	angLeft = 0.0 # positive direction: clockwise: Left - Right
 	angRight = 0.0
 	angAbs = 0.0 # Absolute rotation angle
 	
@@ -112,7 +162,6 @@ class SI(object):
 		    if self.tf.frameExists("/odom") and self.tf.frameExists("/base_footprint"):
 			t = self.tf.getLatestCommonTime("/odom", "/base_footprint")
 			self.tb_pos = self.tf.lookupTransform("/odom", "/base_footprint", t)[0]
-		    #print self.tb_vel
 		    
 		    self.pubTBCommand.publish(self.tb_vel)
 		    #rospy.spin()
